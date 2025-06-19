0812b32728844c42ae0ef0820869d16bf3fb2d5e
Buildra <buildra@protonmail.com>
2025-06-19 09:01:53 +0200

    update

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-rm kiro_repo*
+rm buildra_repo*
 
 echo "repo-add"
-repo-add -n -R kiro_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R buildra_repo.db.tar.gz *.pkg.tar.zst
 
 sleep 1
 
-rm kiro_repo.db
+rm buildra_repo.db
 
-rm kiro_repo.files
+rm buildra_repo.files
 
-mv kiro_repo.db.tar.gz kiro_repo.db
+mv buildra_repo.db.tar.gz buildra_repo.db
 
-mv kiro_repo.files.tar.gz kiro_repo.files
+mv buildra_repo.files.tar.gz buildra_repo.files
 
 echo "####################################"
 echo "Repo Updated!!"
